bb0cd4cb7dc5f34be1018a50bc28087e2781f434
ico1036 <jiwoong.kim@cern.ch>
2020-10-12 11:08:37 +0900

    m

---
+++ Sample_Generation/CMS_workFlow/CMSSW_10_2_5/.SCRAM/slc6_amd64_gcc700/MakeData/Tools/herwigpp.mk
@@ -0,0 +1,5 @@
+ALL_TOOLS      += herwigpp
+herwigpp_EX_INCLUDE := /cvmfs/cms.cern.ch/slc6_amd64_gcc700/external/herwigpp/7.1.2-gnimlf8/include
+herwigpp_EX_LIB := HerwigAPI
+herwigpp_EX_USE := root_cxxdefaults lhapdf thepeg madgraph5amcatnlo openloops
+
